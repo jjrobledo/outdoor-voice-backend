63217585d61969cc106a3731ee124add941aaf1f
jrobledo <robledo.josej@gmail.com>
2022-09-05 12:37:09 -0600

    fixed wrong request method for user login route

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const { loginUser, registerUser } = require("../controllers/users.controllers");
 
+router.post("/login", loginUser);
 router.post("/register", registerUser);
-
-router.get("/login", loginUser);
 
 module.exports = router;
