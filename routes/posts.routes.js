bf20ded73056a5fef26ec8e5e185a3c60892bf4b
jrobledo <robledo.josej@gmail.com>
2022-09-05 09:18:52 -0600

    added skeleton code for protecting post routes and update and delete routes

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
+const authenticate = require("../middleware/auth.middleware");
 const { getAllPosts, createPost } = require("../controllers/posts.controllers");
+
+// TODO
+// - protect routes with auth middelware
+// - add patch and delete routes
+
+// router.use(authenticate);
+
+router.get("/", getAllPosts);
 
 router.post("/", createPost);
 
-router.get("/", getAllPosts);
+// router.delete("/:id", deletePost);
+// router.patch("/:id", updatePost)
 
 module.exports = router;
