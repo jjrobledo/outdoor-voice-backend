c1453632cbb6264de409cc2a1cb57547669adc2e
jrobledo <robledo.josej@gmail.com>
2022-09-27 08:55:22 -0600

    added multer to server.js

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-const express = require("express");
+const cloudinary = require("cloudinary");
 const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require("express");
 const morgan = require("morgan");
+const multer = require("multer");
+const cors = require("cors");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+const storage = multer.diskStorage({
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  },
+});
+
+const imageFilter = function (req, file, callback) {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    return callback(new Error("Error: file must be a vaild image"), false);
+  }
+  callback(null, true);
+};
+
+const upload = multer({ storage: storage, fileFilter: imageFilter });
 
 const postRouter = require("./routes/posts.routes.js");
 const userRouter = require("./routes/users.routes.js");
