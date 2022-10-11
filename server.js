c11b234c8642948cd78815551966978b2f325810
jrobledo <robledo.josej@gmail.com>
2022-10-10 20:28:52 -0600

    removed multer as dependency

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const cloudinary = require("./cloudinary/cloudinary");
 const mongoose = require("mongoose");
 const express = require("express");
 const morgan = require("morgan");
-const multer = require("multer");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -10,25 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-const storage = multer.diskStorage({
-  filename: function (req, file, callback) {
-    callback(null, Date.now() + file.originalname);
-  },
-});
-
-const imageFilter = function (req, file, callback) {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
-    return callback(new Error("Error: file must be a vaild image"), false);
-  }
-  callback(null, true);
-};
-
-const upload = multer({ storage: storage, fileFilter: imageFilter });
-
 const postRouter = require("./routes/posts.routes.js");
 const userRouter = require("./routes/users.routes.js");
 
-//app.use(cors());
+app.use(cors());
 
 app.use(morgan("dev"));
 app.use(express.json({ limit: "5mb" }));
