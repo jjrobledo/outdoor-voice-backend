a479ff6483a8d10561ae636f87ad0cc1eb04f3c6
Jesse Robledo, Web Developer <robledo.josej@gmail.com>
2022-10-23 20:24:15 -0600

    Updated server.js for cloudinary

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const cloudinary = require("cloudinary");
 const mongoose = require("mongoose");
 const express = require("express");
 const morgan = require("morgan");
-const multer = require("multer");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -10,33 +8,14 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-cloudinary.config({
-  cloud_name: "dvr2dt9kh",
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const storage = multer.diskStorage({
-  filename: function (req, file, callback) {
-    callback(null, Date.now() + file.originalname);
-  },
-});
-
-const imageFilter = function (req, file, callback) {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
-    return callback(new Error("Error: file must be a vaild image"), false);
-  }
-  callback(null, true);
-};
-
-const upload = multer({ storage: storage, fileFilter: imageFilter });
-
 const postRouter = require("./routes/posts.routes.js");
 const userRouter = require("./routes/users.routes.js");
 
 app.use(cors());
+
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
